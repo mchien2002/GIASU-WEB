4e5d0d8844819591145481a4ecb762f1b91e6fd4
unknown <minhchien77777@gmail.com>
2022-10-15 00:54:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./resources/assets/css/bootstrap.min.css">
+    <title>TRUNG TÂM GIA SƯ</title>
+    <script type="module" src="/resources/assets/js/main.js"></script>
+    <link rel="stylesheet" href="/resources/assets/css/header.css">
+    <link rel="stylesheet" href="/resources/assets/css/footer.css">
+    <link rel="stylesheet" href="/resources/assets/css/sidebar.css">
+    <link rel="stylesheet" href="/resources/assets/css/style.css">
+    <link rel="stylesheet" href="/resources/assets/css/home.css">
+</head>
+<body>
+    <div id="wrapper">
+        <?php  
+            require './views/layout/header.php';
+            require './views/page/home.php';
+            require './views/layout/footer.php';
+            require './views/layout/sidebar.php';
+        ?>
+    </div>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous"></script>
+    <script src="./resources/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ views/layout/header.php
@@ -0,0 +1,25 @@
+<div id="header">
+    <div id="banner"></div>
+    <div id="menu-top">
+        <div class="ddsmoothmenu" id="smoothmenu1">
+            <ul>
+                <li><a href="#" class="active">Trang chủ</a></li>
+                <li style="z-index: 100;"><a href="#">Giới thiệu</a></li>
+                <li style="z-index: 100;"><a href="#" class="">Lớp mới</a></li>
+                <li style="z-index: 100;"><a href="#">Bảng lương</a>
+                </li>
+                <li style="z-index: 100;" class="nav-giasu nav-item dropdown">
+                    <a href="#">Gia sư<img class="downarrowclass"></a>
+                    <ul class="nav-menu dropdown-menu fade-up">
+                        <li><a href="#" class="dropdown-item">Đăng ký làm gia sư</a></li>
+                        <li><a href="#" class="dropdown-item">Quy trình nhận lớp</a></li>
+                        <li><a href="#" class="dropdown-item">Trung tâm gia sư uy tín</a></li>
+                    </ul>
+                </li>
+                <li style="z-index: 100;"><a href="#">Tuyển dụng</a></li>
+                <li style="z-index: 100;"><a href="#">Mức phí gia sư</a></li>
+                <li style="background:none"><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
